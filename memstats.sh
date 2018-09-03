657193cb0d1da0853e439c782c69e32ab4b1701c
aks <42908045+a93-git@users.noreply.github.com>
2018-09-04 02:29:12 +0530

    Fix mem-stat file issue

--- memstats.sh
+++ memstats.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 memTot=$(cat /proc/meminfo | grep 'MemTotal' | sed -e 's/ //g' -e 's/kB//g' | cut -f 2 -d ':')
 memFree=$(cat /proc/meminfo| grep 'MemFree' | sed -e 's/ //g' -e 's/kB//g' | cut -f 2 -d ':')
-echo "(($memTot-$memFree)*100)/$memTot" | bc
+a=$(echo "($memTot-$memFree)*100/$memTot" | bc)
+echo $a > mem-stat
