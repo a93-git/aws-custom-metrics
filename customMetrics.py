68ce4c56dd378242787840fd473aede9a72559b7
aks <42908045+a93-git@users.noreply.github.com>
2018-09-03 23:24:46 +0530

    Add metric name constants

--- customMetrics.py
+++ customMetrics.py
@@ -3,8 +3,11 @@ TODO: Add more details in the header
 TODO: Add usage
 """
 
+# Constants used in the program
+DISK_METRIC = "DiskUtil"
+MEM_METRIC = "MemUtil"
+
 from datetime import datetime
 
 # Retrieve the current timestamp
 timestamp = datetime.timestamp(datetime.now())
-
