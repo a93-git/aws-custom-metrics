07794b880c6dca0d8a6699dfb01a9d0672bf2227
aks <42908045+a93-git@users.noreply.github.com>
2018-09-04 02:45:14 +0530

    Remove deleted files

--- memstats.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-memTot=$(cat /proc/meminfo | grep 'MemTotal' | sed -e 's/ //g' -e 's/kB//g' | cut -f 2 -d ':')
-memFree=$(cat /proc/meminfo| grep 'MemFree' | sed -e 's/ //g' -e 's/kB//g' | cut -f 2 -d ':')
-a=$(echo "($memTot-$memFree)*100/$memTot" | bc)
-echo $a > mem-stat
