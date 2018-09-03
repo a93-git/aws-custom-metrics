311f7e2bc831081ed422fc5fb1e94f61bc7174be
aks <42908045+a93-git@users.noreply.github.com>
2018-09-03 23:43:10 +0530

    Add function to put memory utilization metric

--- customMetrics.py
+++ customMetrics.py
@@ -6,8 +6,37 @@ TODO: Add usage
 # Constants used in the program
 DISK_METRIC = "DiskUtil"
 MEM_METRIC = "MemUtil"
+NAMESPACE = "Boto/Metric"
+UNIT_MEM = 'Percent'
+
+with open('instance-id' as f):
+    INSTANCE_ID = f.readline()
 
 from datetime import datetime
+import subprocess
 
 # Retrieve the current timestamp
 timestamp = datetime.timestamp(datetime.now())
+
+def put_mem_metric_data():
+    """ Sends memory metric data to cloudwatch """
+    response = client.put_metric_data(
+        Namespace=NAMESPACE,
+        MetricData=[
+            {
+                'MetricName': MEM_METRIC,
+                'Dimensions': [
+                    {
+                        'Name': 'InstanceId',
+                        'Value': INSTANCE_ID
+                    }
+                ],
+                'Timestamp': timestamp,
+                'Value': 
+                'Unit': UNIT_MEM,
+                'StorageResolution': 60
+            }
+        ]
+    )
+
+
