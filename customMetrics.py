6a0046f238664a1aa8fcddb5ea1d87595218457f
aks <42908045+a93-git@users.noreply.github.com>
2018-09-04 02:20:40 +0530

    Fix the filepath issue and add disk metric function

--- customMetrics.py
+++ customMetrics.py
@@ -8,15 +8,29 @@ DISK_METRIC = "DiskUtil"
 MEM_METRIC = "MemUtil"
 NAMESPACE = "Boto/Metric"
 UNIT_MEM = 'Percent'
+UNIT_DISK = 'Percent'
 
 from datetime import datetime
 import subprocess
 import boto3
+import os
 
-subprocess.call("./instanceid.sh", shell=True)
 
-with open('instance-id', 'r') as f:
-    INSTANCE_ID = f.readline()
+currentDir = os.getcwd()
+instanceIdPath = currentDir + '/instance-id'
+instanceScript = "source " + currentDir + '/instanceid.sh'
+memStatPath = currentDir + '/mem-stat'
+memStatScript = "source " + currentDir + '/memstat.sh'
+
+diskStatPath = currentDir + '/disk-stat'
+diskStatScript = "source " + currentDir + '/disk.sh'
+
+subprocess.call(instanceScript, shell=True)
+try:
+    with open(instanceIdPath, 'r') as f:
+        INSTANCE_ID = f.readline()
+except:
+    print('Error here')    
 
 # Retrieve the current timestamp
 timestamp = datetime.timestamp(datetime.now())
@@ -25,9 +39,13 @@ cloudwatch_client = boto3.client('cloudwatch')
 
 def put_mem_metric_data():
     """ Sends memory metric data to cloudwatch """
-    subprocess.call("./memstat.sh", shell=True)
-    with open('mem-stat', 'r') as f:
-        mem_value = f.readline()
+    subprocess.call(memStatScript, shell=True)
+    
+    try:
+        with open(memStatPath, 'r') as f:
+            mem_value = f.readline()
+    except:
+        print('Error memstat')
 
     response = cloudwatch_client.put_metric_data(
         Namespace=NAMESPACE,
@@ -38,18 +56,20 @@ def put_mem_metric_data():
                     {
                         'Name': 'InstanceId',
                         'Value': INSTANCE_ID
-                    }
-                ],
+                    }   
+                ],  
                 'Timestamp': timestamp,
                 'Value': float(mem_value),
                 'Unit': UNIT_MEM,
                 'StorageResolution': 60
-            }
+            }   
         ]
     )
 
-def put_disk_metric_data():
-    """ Sends disk metric data to cloudwatch """
+put_mem_metric_data()
+
+def put_disk_metric_data(mountpoint, disk_value):
+    """ Sends disk metric data to cloudwatach """
     response = cloudwatch_client.put_metric_data(
         Namespace=NAMESPACE,
         MetricData=[
@@ -61,8 +81,28 @@ def put_disk_metric_data():
                         'Value': INSTANCE_ID
                     },
                     {
-                        'Name': 
+                        'Name': 'Mountpoint',
+                        'Value': mountpoint
+                    }           
+                ],
+                'Timestamp': timestamp,
+                'Value': float(disk_value),
+                'Unit': UNIT_DISK,
+                'StorageResolution': 60
+            }
+        ]
+    )
+    
 
+def startDisk():
+    subprocess.call(diskStatScript, shell=True)
+    temp = []
+    with open(diskStatPath, 'r') as f:
+        for i in f.readlines():
+            temp.append(i)
 
+    for i in temp:
+        z = i.strip().split(sep='\t')
+        put_disk_metric_data(z[1], z[0])
 
-put_mem_metric_data()
+startDisk()
