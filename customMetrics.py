83498ceef2d0ae173c484d75e0558e48f18d97ee
aks <42908045+a93-git@users.noreply.github.com>
2018-09-03 23:20:46 +0530

    Add timestamp function

---
+++ customMetrics.py
@@ -0,0 +1,10 @@
+"""Send custom metric data to cloudwatch
+TODO: Add more details in the header
+TODO: Add usage
+"""
+
+from datetime import datetime
+
+# Retrieve the current timestamp
+timestamp = datetime.timestamp(datetime.now())
+
